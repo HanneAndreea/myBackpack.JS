fbf34d2d61c320b238dfc4dced3312e5b79d6d2f
HanneAndreea <62730815+HanneAndreea@users.noreply.github.com>
2020-04-01 20:25:26 -0700

    Add files via upload

---
+++ myBackpack.script.js
@@ -0,0 +1,6 @@
+var myBackpack = { food: [ `bananas`,`apples`,`sandwiches`],
+                  equipment: [`map`,`flashlight`,`matches`,`compass`],
+                  clothing: [`hat`,`sweater`,`scarf`,`gloves`,`socks`],};
+print(myBackpack.food);
+print(myBackpack.equipment);
+print(myBackpack.clothing);
